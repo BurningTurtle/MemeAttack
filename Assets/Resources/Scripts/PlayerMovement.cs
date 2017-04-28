61b067c24c3182ec726bd6456b2d4abc538cc7aa
Robyn K <robynkoelle@gmail.com>
2017-04-28 21:45:00 +0200

    Added Player and Dolan
    Added Dolan Sprites (without animation)
    Added (boring :P) Player Sprites
    Added Player Idle Animation
    Added Player Walking Animation
    Added Player Movement

---
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //Show this variable in inspector
+    [SerializeField] private float speed = 3f;
+
+    //private bool isLink = false;
+    private bool alive = true;
+
+    private Animator animation;
+
+    private void Start()
+    {
+        animation = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (alive)
+        {
+            float xValue = Input.GetAxis("Horizontal") * speed;
+            float yValue = Input.GetAxis("Vertical") * speed;
+            Vector2 movement = new Vector2(xValue, yValue);
+            movement = Vector2.ClampMagnitude(movement, speed); //limit diagonal movement to the same speed as movement along an axis
+
+            movement *= Time.deltaTime;
+
+            animation.SetBool("isWalking", false); //so that player doesn't continue running once he was moving
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)  
+            {
+                animation.SetBool("isWalking", true); //switches from idle to walk animation
+                transform.Translate(movement);
+            }
+            
+        }
+    }
+}
