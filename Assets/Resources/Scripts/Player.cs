8e3171f00920085a1f33764fe9ecf7e64d64ec03
Moritz <moritz.unseld@googlemail.com>
2017-04-29 20:55:20 +0200

    Added PlayerProjectiles and many improvements
    Added PlayerProjectile, improved general projectile behavior, improved damage system and explained many things through comments.

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -7,13 +7,15 @@ public class Player : MonoBehaviour
 
     // Show this variable in inspector
     [SerializeField] private float speed = 3f;
-
     public int health;
-    private bool readyForDamage = true;
 
     private bool alive = true;
+    private bool readyForDamage = true;
+
 
     private Animator anim;
+   [SerializeField] private GameObject projectilePrefab;
+
 
     private void Start()
     {
@@ -47,6 +49,11 @@ public class Player : MonoBehaviour
 
             Debug.Log(health);
 
+            if (Input.GetMouseButtonDown(0)) //&& !projectileFlying)
+            {
+                StartCoroutine(shoot());
+            }
+
         }
 
        
@@ -54,16 +61,11 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "MainEnemyProjectile")
-        {
-            health -= 1;
-            Destroy(collision.gameObject);
-        }
-
         if (readyForDamage)
         {
             switch (collision.gameObject.tag)
             {
+                // Apply colliding damage according to enemy.
                 case "MainEnemy":
                     health -= 1;
                     break;
@@ -73,11 +75,12 @@ public class Player : MonoBehaviour
                 case "DatBoi":
                     health -= 3;
                     break;
-                
             }
             StartCoroutine(getReadyForDamage());
-        }        
+        }  
     }
+
+    
 
     // Damage the player every second if the enemy sticks with him. (Avoiding way too much damage)
     IEnumerator getReadyForDamage()
@@ -85,5 +88,36 @@ public class Player : MonoBehaviour
         readyForDamage = false;
         yield return new WaitForSeconds(1.0f);
         readyForDamage = true;
+    }
+
+    IEnumerator shoot()
+    {
+        // Get mouse position.
+        Vector3 mousePos = Input.mousePosition;
+
+        // Get player position.
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        // Get mouse position relative to the player.
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        // Convert x and y into an angle using built in functions.
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        // Creating a direction vector for our projectile.
+        Vector3 dir = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+
+        // Creating the projectile and move it into the player.
+        GameObject projectile = Instantiate(projectilePrefab) as GameObject;
+        projectile.transform.position = this.transform.position;
+
+        // Add the calculated force.
+        projectile.GetComponent<Rigidbody2D>().AddForce(dir / 10);
+
+        // Destory the projectile if it didn't hit anything after 2 seconds.
+        yield return new WaitForSeconds(2f);
+        Destroy(projectile.gameObject);
+
     }
 }
