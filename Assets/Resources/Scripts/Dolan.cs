6a3b70aa4c4f02bead6cc0ff3f1b4d82eedff785
Robyn K <robynkoelle@gmail.com>
2017-04-29 13:30:51 +0200

    Dolan & Main Enemy
    Dolan and Main Enemy now follow the player without losing speed

---
+++ Assets/Resources/Scripts/Dolan.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dolan : MonoBehaviour {
+
+    private bool alive = true;
+    private float speed = 2f;
+    [SerializeField] private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        if (alive)
+        {
+            Vector2 playerVector = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
+            GetComponent<Rigidbody2D>().velocity = playerVector * speed * Time.deltaTime;
+
+            while(GetComponent<Rigidbody2D>().velocity.magnitude < speed)
+            {
+                GetComponent<Rigidbody2D>().velocity *= 1.1f;
+            }
+        }
+    }
+}
