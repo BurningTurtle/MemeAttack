35c55ae23be085551b5642417650081b10e7eeb9
Robyn K <robynkoelle@gmail.com>
2017-06-03 14:08:53 +0200

    Added DatDolan (to be finished): Animation, First Attack, Health etc.

---
+++ Assets/Resources/Scripts/PileOfCotton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PileOfCotton : MonoBehaviour {
+
+    private GameObject player;
+    private int damage = 20;
+    private SoundManager soundMan;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        soundMan = FindObjectOfType<SoundManager>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            other.GetComponent<Player>().health -= damage;
+            soundMan.playCottonDamage();
+            Destroy(gameObject);
+        }
+    }
+}
