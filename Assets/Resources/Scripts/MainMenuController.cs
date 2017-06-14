f99ef31f3d844df76a671a3137c9e627031458d7
Moritz <moritz.unseld@googlemail.com>
2017-06-14 20:33:48 +0200

    Finished MainMenu

--- Assets/Resources/Scripts/MainMenuController.cs
+++ Assets/Resources/Scripts/MainMenuController.cs
@@ -12,6 +12,8 @@ public class MainMenuController : MonoBehaviour {
     private GameObject[] bars;
     [SerializeField]
     private GameObject canvas;
+    [SerializeField]
+    private GameObject background;
 
     // Use this for initialization
     void Start () {
@@ -31,7 +33,9 @@ public class MainMenuController : MonoBehaviour {
             Vector3 previousScale = bars[i].transform.localScale;
             previousScale.y = Mathf.Lerp(previousScale.y, spectrum[i] * 16, Time.deltaTime * 30);
             bars[i].transform.localScale = previousScale;
-        } 
+        }
+        animateBackground();
+        background.transform.Rotate(0, 0, 90 * Time.deltaTime);
     }
 
     public void loadGame()
@@ -49,5 +53,27 @@ public class MainMenuController : MonoBehaviour {
             bar.SetActive(true);
         }
         canvas.SetActive(true);
+    }
+
+    private void animateBackground()
+    {
+
+        float[] spectrum = AudioListener.GetSpectrumData(1024, 0, FFTWindow.Hamming);
+
+        Vector3 previousScale = background.transform.localScale;
+
+        //if(Mathf.Lerp(previousScale.y, (spectrum[2] + spectrum[3] + spectrum[4] + spectrum[5] + spectrum[6]) * 30, Time.deltaTime * 30) < 4.5f && Mathf.Lerp(previousScale.y, (spectrum[2] + spectrum[3] + spectrum[4] + spectrum[5] + spectrum[6]) * 30, Time.deltaTime * 30) > 3)
+        //{
+            previousScale.y = Mathf.Lerp(previousScale.y, (spectrum[2] + spectrum[3] + spectrum[4] + spectrum[5] + spectrum[6]) * 45, Time.deltaTime * 30);
+        //} 
+
+        //if (Mathf.Lerp(previousScale.x, (spectrum[2] + spectrum[3] + spectrum[4] + spectrum[5] + spectrum[6]) * 30, Time.deltaTime * 30) < 4.5f && Mathf.Lerp(previousScale.x, (spectrum[2] + spectrum[3] + spectrum[4] + spectrum[5] + spectrum[6]) * 30, Time.deltaTime * 30) > 3)
+        //{
+            previousScale.x = Mathf.Lerp(previousScale.x, (spectrum[2] + spectrum[3] + spectrum[4] + spectrum[5] + spectrum[6]) * 45, Time.deltaTime * 30);
+        //} 
+
+
+
+        background.transform.localScale = previousScale;
     }
 }
